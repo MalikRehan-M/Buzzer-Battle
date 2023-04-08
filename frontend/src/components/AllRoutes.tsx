448a189eee70b79bb09408925eceab831fcee219
PoojaRathod5 <poojachavan8669@gmail.com>
2023-04-08 20:40:22 +0530

    add frontpage

--- frontend/src/components/AllRoutes.tsx
+++ frontend/src/components/AllRoutes.tsx
@@ -2,12 +2,14 @@
 import Singleplayer from "../pages/singleplayer";
 import { Route, Routes } from "react-router-dom";
 import Multiplayer from "../pages/multiplayer";
+import FrontPage from "../pages/frontPage";
 
 const AllRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<FrontPage />} />
       <Route path="/singleplayer" element={<Singleplayer />} />
-      <Route path="/multiplayer" element={<Multiplayer/>} />
+      <Route path="/multiplayer" element={<Multiplayer />} />
     </Routes>
   );
 };
---
+++ frontend/src/pages/frontPage.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import "./frontPage.css"; // import the CSS file
+import 'animate.css';
+
+const FrontPage: React.FC = () => {
+
+    const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!isDropdownOpen);
+    };
+    const [selectedOption, setSelectedOption] = useState("");
+
+    // Add event handler for option selection
+    const handleOptionSelect = (option: string) => {
+      setSelectedOption(option);
+      setDropdownOpen(!isDropdownOpen)
+    };
+  
+
+    return (
+        <div className="front-page-container"> {/* Use className to apply CSS class */}
+            <h1 className="front-page-title">Home Page</h1>
+            <br />
+            <div className="bar">
+                <span className="bar_content">
+                    Welcome to the quiz game
+                </span>
+            </div>
+            <div className="button-container">
+              
+                <div className="button-wrapper">
+                    <button
+                        className="custom-button "
+                    >
+                        Single Player
+                    </button>
+                    <button className="custom-button">
+                        Multi Player
+                    </button>
+                </div>
+                <div className="button-wrapper">
+                    <button className="custom-button">
+                        Number of players
+                    </button>
+                    <button
+                        className="custom-button"
+                        onClick={toggleDropdown}
+                    >
+                        {selectedOption? `Categories : ${selectedOption}` : "Select Category"}
+                    </button>
+                    {isDropdownOpen && (
+                         <div className="dropdown">
+                         <ul>
+                           <li onClick={() => handleOptionSelect("Entertainment")}>Entertainment</li>
+                           <li onClick={() => handleOptionSelect("Technical")}>Technical</li>
+                           <li onClick={() => handleOptionSelect("General")}>General</li>
+                         </ul>
+                         {/* Render selected option or perform action with selected option */}
+                        
+                       </div>
+                    )}
+                    <button className="custom-button">
+                        Create
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
+export default FrontPage;
